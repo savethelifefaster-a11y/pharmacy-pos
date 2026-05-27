bbc4fea48060611878636a18979b8d21e44ae889
savethelifefaster-a11y <savethelifefaster@gmail.com>
2026-05-27 18:43:28 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,6 @@
+localforage.config({ name: 'PharmaPOS_PRO', storeName: 'pharmacy_data' });
+const DB = {
+    getTable: async (t) => (await localforage.getItem(t)) || [],
+    saveTable: async (t, d) => await localforage.setItem(t, d)
+};
 
